d96340a5c71e53e810ed9787162a5dce1f025a9a
CESAR JOSE VASQUEZ GONZALEZ <ISNP024924@ugb.edu.sv>
2024-11-03 23:44:53 -0600

    Agregar archivos de proyecto.

---
+++ academica/Models/Alumno.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace academica.Models {
+    public class Alumno {
+        [Key]
+        public int idAlumno { get; set; }
+        public string Titulo { get; set; }
+        public string Director { get; set; }
+        public string Sinopsis { get; set; }
+        public string Duracion { get; set; }
+        public string Clasificacion { get; set; }
+    }
+}
---
+++ academica/Models/MyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using academica.Models;
+namespace academica.Models {
+    public class MyDbContext : DbContext{
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+        public DbSet<Alumno> Alumnos { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Alumno>().HasKey(alumno => alumno.idAlumno);
+
+        }
+    }
+}
